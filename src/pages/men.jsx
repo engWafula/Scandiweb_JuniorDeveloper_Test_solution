3733d921107383f6f2c4edb65491d047f32b9451
engWafula <wafulaallan5@gmail.com>
2022-03-30 19:39:45 +0300

    initial commit

---
+++ src/pages/men.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+class men extends React.Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+}
+
+    render() {
+        return <div>hello</div>;
+    }
+}
+
+men.propTypes = propTypes;
+men.defaultProps = defaultProps;
+// #endregion
+
+export default men;
--- src/pages/women.jsx
+++ src/pages/women.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class women extends Component {
   render() {
     return (
       <div>
-        hello
+        heloo
       </div>
     )
   }
